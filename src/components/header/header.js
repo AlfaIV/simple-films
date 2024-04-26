c2c9221ae66692dc789310d4aff4e78d3a265b3a
alfaiv <alfaiv@yandex.ru>
2024-04-26 22:51:30 +0300

    connect scss

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import './header.scss'
+
+const MyHeader = () => {
+    return (
+        <div className='header'>
+            <p className='header__text'>
+                Кино справочник
+            </p>
+        </div>
+    )
+}
+
+export default MyHeader
---
+++ src/views/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyHeader from '../../components/header/header';
+
+const App =() => {
+  return (
+    <div className="App">
+      <MyHeader/> 
+    </div>
+  );
+}
+
+export default App;
