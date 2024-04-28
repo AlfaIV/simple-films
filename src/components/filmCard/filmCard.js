1103042c65ac30ae4122092cfc7ee4a7b81bcdba
alfaiv <alfaiv@yandex.ru>
2024-04-28 13:52:05 +0300

    modify filmCard

--- src/components/filmCard/filmCard.js
+++ src/components/filmCard/filmCard.js
@@ -1,32 +1,13 @@
 import "./filmCard.scss"
-import { useDispatch, useSelector } from "react-redux";
 
-const FilmCard = () => {
-    const dispatch = useDispatch();
-    const film = useSelector(state => state.listFilms[0]);
-    console.log(film)
-    const {id, title, url, rating} = film;
+const FilmCard = ({id, title, url, rating, clickHandler}) => {
     
-    const handlerClick = (e) => {
-        const targetClass = e.currentTarget;
-        switch (targetClass) {
-            case '1':
-                break;
-            case '2':
-                break;
-            case '3':
-                break;
-            default:
-                console.log(targetClass);
-        }
-    }
-
     return (
         <div
             className='film-selection__film'
             data-section={id}
             draggable='true'
-            onClick={handlerClick}
+            onClick={clickHandler}
         >
             <div
                 className='film-selection__film-poster'
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -3,14 +3,33 @@ import MyHeader from '../../components/header/header.js';
 import MyFooter from '../../components/footer/footer';
 import FilmCard from '../../components/filmCard/filmCard.js';
 import Description from '../../components/description/description.js';
+import { useDispatch, useSelector } from "react-redux";
 
-const App =() => {
+const App = () => {
+
+  const dispatch = useDispatch();
+  const films = useSelector(state => state.listFilms);
+  console.log(films)
+
+  const handlerClick = (e) => {
+    const targetClass = e.currentTarget;
+    console.log(targetClass);
+  }
+
   return (
     <div className="App">
-      <MyHeader/> 
-      <FilmCard/>
+      <MyHeader />
+      {films.map(film =>
+        <FilmCard
+          id={film.id}
+          title={film.title}
+          url={film.url}
+          rating={film.rating}
+          clickHandler={handlerClick}
+        />
+      )}
       {/* <Description/> */}
-      <MyFooter/> 
+      <MyFooter />
     </div>
   );
 }
